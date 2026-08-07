7f2ceba3af8e19b08c2d07191d4278bd34b1c35e
trnvra <tarunv281@gmail.com>
2026-08-08 00:24:58 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,10 @@ require("dotenv").config() // ✅ Sabse pehle yeh
 
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
+const PORT = process.env.PORT || 3000;
 
 connectToDB()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser()) 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
 }))
 
 const authRouter = require("./routes/auth.routes")
